307276257dc79fbc75a11c0fad996badd34bf186
Dacheng Li <dal177@ucsd.edu>
2023-05-17 21:32:19 +0400

    attn span

--- attention_span.py
+++ attention_span.py
@@ -3,15 +3,20 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from utils import load_model, load_testcases, test, attention_span
 
-path = "/data/dacheng/longchat_7b_4096"
-test_file = "evaluation/topics/testcases/3_topics.jsonl"
+torch.set_printoptions(profile="full")
 
-model, tokenizer = load_model(path)
+path = "/data/dacheng/longchat_7b_4096"
+test_file = "evaluation/topics/testcases/5_topics.jsonl"
+
+model, tokenizer = load_model(path, device="cuda", num_gpus=8)
 
 test_cases = load_testcases(test_file)
 
 for i, test_case in enumerate(test_cases):
-    span, span_avg_layer, span_avg_all = attention_span(model, tokenizer, testcase)
-    print(f"---------- Testcase {i} --------")
+    prompt = test_case["prompt"]
+    prompt_length = test_case["prompt_length"]
+    topics = test_case["topics"]
+    span, span_avg_layer, span_avg_all = attention_span(model, tokenizer, test_case)
+    print(f"---------- Testcase {i} length {prompt_length} ----")
     print(span, span_avg_layer, span_avg_all)
     assert False
--- utils.py
+++ utils.py
@@ -5,8 +5,21 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from matplotlib import pyplot as plt
 
-def load_model(path, dtype=torch.bfloat16, device="cuda"):
-    model = AutoModelForCausalLM.from_pretrained(path, torch_dtype=dtype).to(device)
+def load_model(path, dtype=torch.bfloat16, device="cuda", num_gpus=1):
+    if device == "cpu":
+        kwargs = {"torch_dtype": torch.float32}
+    elif device == "cuda":
+        kwargs = {"torch_dtype": torch.bfloat16}
+        if num_gpus != 1:
+            kwargs["device_map"] = "auto"
+            kwargs["device_map"] = "sequential"  # This is important for not the same VRAM sizes
+            # Hard code for A100s
+            available_gpu_memory = [3] * num_gpus
+            kwargs["max_memory"] = {
+                    i: str(int(available_gpu_memory[i] * 0.85)) + "GiB"
+                    for i in range(num_gpus)
+                }
+    model = AutoModelForCausalLM.from_pretrained(path, low_cpu_mem_usage=True, **kwargs)
     tokenizer = AutoTokenizer.from_pretrained(path)
     return model, tokenizer
 
@@ -17,7 +30,7 @@ def load_testcases(test_file):
     test_cases = []
     for test_case in json_list:
         test_case = json.loads(test_case)
-        test_case_list.append(test_case)
+        test_cases.append(test_case)
 
     return test_cases
 
@@ -26,7 +39,7 @@ def test(test_case, model, tokenizer, return_summary=True):
     prompt_length = test_case["prompt_length"]
     topics = test_case["topics"]
     input = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(input.input_ids.cuda(), max_new_tokens=100, use_cache=True)[0]
+    outputs = model.generate(input.input_ids.to(model.device), max_new_tokens=100, use_cache=True)[0]
     outputs = outputs[prompt_length:]
     outputs = tokenizer.batch_decode([outputs], skip_special_tokens=True)
     if return_summary:
@@ -35,9 +48,14 @@ def test(test_case, model, tokenizer, return_summary=True):
     else: 
         return outputs
 
-def attention_span(model, tokenizer, testcase, num_steps=1):
+def attention_span(model, tokenizer, test_case, num_gen_steps=1, raw_attn=False):
     assert num_gen_steps == 1, "Only support span for a single generation step now."
-    output = model(input, output_attentions=True)
+    prompt = test_case["prompt"]
+    prompt_length = test_case["prompt_length"]
+    topics = test_case["topics"]
+    input = tokenizer(prompt, return_tensors="pt")
+
+    output = model(input.input_ids.to(model.device), output_attentions=True)
     num_layer = len(output.attentions)
 
     attn_mat = torch.cat([a[0, :, -1, :] for a in output.attentions])
@@ -50,7 +68,10 @@ def attention_span(model, tokenizer, testcase, num_steps=1):
     span_avg_layer = torch.mean(span, dim=1)
     span_avg_all = torch.mean(span)
 
-    return span, span_avg_layer, span_avg_all
+    if raw_attn:
+        return span, span_avg_layer, span_avg_all, attn_mat
+    else:
+        return span, span_avg_layer, span_avg_all
 
 def visualize_attn(attn_mat, save_path):
     pass
