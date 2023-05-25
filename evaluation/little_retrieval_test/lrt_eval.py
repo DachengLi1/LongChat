0679f19a42c1149f4d9313893b4568e0e5b667a9
AnzeXie <a1xie@ucsd.edu>
2023-05-25 10:22:38 +0400

    lrt codes and some results

--- evaluation/little_retrieval_test/lrt_eval.py
+++ evaluation/little_retrieval_test/lrt_eval.py
@@ -10,11 +10,31 @@ import re
 import transformers
 import torch
 
+def get_tokenizer_model_from_ckpt(cfg):
+    ckpt_path = cfg["ckpt_path"]
 
+    config = transformers.MPTConfig.from_pretrained(ckpt_path)
+    config.attn_config['attn_impl'] = 'torch'
+
+    model = transformers.MPTForCausalLm.from_pretrained(ckpt_path, 
+                                                        config=config, 
+                                                        torch_dtype=torch.bfloat16)
+    # model.to(device='cuda:1')
+    model = model.to(torch.bfloat16)
+
+    tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b",
+                                                            model_max_length=4096,
+                                                            padding_side="right")
+
+    return tokenizer, model
 
 def retrieve_expected(lines, random_line_pos):
     correct_line = lines[random_line_pos]
-    expected_number = re.search("<\d+>", correct_line).group()[1:-1]
+    expected_number = re.search("<\d+>", correct_line)
+    if expected_number is not None:
+        expected_number = int(expected_number.group()[1:-1])
+    else:
+        print(f"Got unparsable line: {correct_line}")
 
     return expected_number, correct_line
 
@@ -48,7 +68,7 @@ def generate_and_modify_text_file(n, shuffle_flag, B, filename=None):
     random_line = random.randint(1, n)
     # add the EXECUTE instruction in random line of the text
     # lines.insert(len(lines), f"[EXECUTE THIS]: Go to line {random_line} and report only REGISTER_CONTENT, without any context or additional text, just the number, then EXIT\n")
-    lines.insert(len(lines), f"What is the REGISTER_CONTENT in line {random_line}?\n")
+    lines.insert(len(lines), f"Tell me what is the REGISTER_CONTENT in line {random_line}? I need the number.\n")
     
     if filename is not None:
         with open(filename, "w") as f:
@@ -68,18 +88,22 @@ def retrieve_cmd_args():
 
 def retrieve_model_response(model, tokenizer, input, lines, random_line_pos, use_gpu):
     if use_gpu:
-        response = model.generate(input.input_ids.cuda(), max_new_tokens=256, use_cache=True)
+        response = model.generate(input.input_ids.cuda(), max_new_tokens=2048, use_cache=True)
     else:
-        response = model.generate(input.input_ids, max_new_tokens=256, use_cache=True)
+        response = model.generate(input.input_ids, max_new_tokens=2048, use_cache=True)
     response = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(response[0]))
     #response = tokenizer.batch_decode([response], skip_special_tokens=True)
 
-    expected_number, correct_line = retrieve_expected(lines, random_line_pos)
+    # response_number = re.search("<\d+>.</s>$", response)
+    response_number = re.findall("\d+", response)
+    if response_number is not None:
+        response_number = int(response_number[-1])
+    else:
+        print(f"Got unparsable result: {response}")
 
-    response_number = re.search("<\d+>", correct_line).group()[1:-1]
     response_line = response
 
-    return expected_number, response_number, correct_line, response_line
+    return response_number, response_line
 
 
 def run_experiment(cfg):
@@ -100,14 +124,15 @@ def run_experiment(cfg):
             model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).cuda()
         else:
             model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
-    else:
+    elif model_path != "None":
         tokenizer = transformers.AutoTokenizer.from_pretrained(model_path, use_fast=False)
 
         if use_gpu:
-            print(torch.cuda.device_count())
             model = transformers.AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16).cuda()
         else:
             model = transformers.AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16)
+    else:
+        tokenizer, model = get_tokenizer_model_from_ckpt(cfg)
     
     tokenizer.pad_token = tokenizer.unk_token
 
@@ -115,11 +140,13 @@ def run_experiment(cfg):
         correct_count = 0
         n_results = []
         curr_results = []
+        token_size = -1
         for i in range(num_eval_per_len):
             print(f"Running eval {i+1}/{num_eval_per_len} for n = {n}...")
             
             # prepare prompt
             lines, random_line = generate_and_modify_text_file(n, shuffle_flag, block_size)
+            expected_number, correct_line = retrieve_expected(lines, random_line)
             prompt = retrieve_prompt_from_lines(lines)
             
             input = tokenizer(prompt, return_tensors="pt")
@@ -128,9 +155,7 @@ def run_experiment(cfg):
             print(f"Number of tokens: {token_size}")
 
             # retrieve model output
-            expected_number, \
             model_output, \
-            correct_line, \
             incorrect_line = retrieve_model_response(model, tokenizer, input, prompt, random_line, use_gpu)
             print(f"Expected number in the prompt: {expected_number}, Model output: {model_output}")
 
@@ -151,22 +176,23 @@ def run_experiment(cfg):
             
         accuracy = (correct_count / num_eval_per_len) * 100
         print(f"Accuracy for n = {n}: {accuracy}%")
-        save_results(cfg, curr_results, accuracy, n)
+        save_results(cfg, curr_results, accuracy, n, token_size)
 
 
-def save_results(cfg, results, accuracy, n):
+def save_results(cfg, results, accuracy, n, token_size):
     name = cfg["model_name"]
-    if name is "None":
-        name = cfg["model_path"].split("/")[0]
+    if name == "None":
+        name = cfg["model_path"].split("/")[-1]
 
-    filename = f"{name}_{n}_non_shuffle_{accuracy}.lrt"
+    filename = f"{name}_{n}_{token_size}_non_shuffle_{accuracy}.lrt"
 
     with open(filename, "w") as f:
         yaml.dump(cfg, f)
         f.write(f"\naccuracy: {accuracy}\n")
+        f.write(f"token_size: {token_size}\n")
 
         for r in results:
-            f.write(r)
+            f.write(f"{r}\n")
 
 def main():
     args = retrieve_cmd_args()
