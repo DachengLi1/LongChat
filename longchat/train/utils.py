ea67fd100d72b69925fccae6ed81e127a64e9846
Dacheng Li <dal177@ucsd.edu>
2023-06-14 03:36:55 +0400

    alibi init

---
+++ longchat/train/utils.py
@@ -0,0 +1,19 @@
+import math
+
+import torch
+
+def get_slopes(n):
+    def get_slopes_power_of_2(n):
+        start = (2**(-2**-(math.log2(n)-3)))
+        ratio = start
+        return [start*ratio**i for i in range(n)]
+
+    if math.log2(n).is_integer():
+        return get_slopes_power_of_2(n)                   #In the paper, we only train models that have 2^a heads for some a. This function has
+    else:                                                 #some good properties that only occur when the input is a power of 2. To maintain that even
+        closest_power_of_2 = 2**math.floor(math.log2(n))  #when the number of heads is not a power of 2, we use this workaround. 
+        return get_slopes_power_of_2(closest_power_of_2) + get_slopes(2*closest_power_of_2)[0::2][:n-closest_power_of_2]
+
+def apply_alibi_bias(attn_mask):
+    bsz, _, q_len, kv_seq_len = attn_mask.shape
+
