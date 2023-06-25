57c80d6ce0f62547d7c0f14f509e6e99712d6379
AnzeXie <a1xie@ucsd.edu>
2023-06-25 06:44:59 +0400

    adapt to original conv_eval_testcases and xformers

--- out_eval.py
+++ out_eval.py
@@ -4,17 +4,21 @@ import re
 import numpy as np
 
 from pathlib import Path
-from util.out_eval_util import *
+from out_eval.out_eval_util import *
 
 from longchat.train.monkey_patch.llama_interpolate_monkey_patch import replace_llama_with_interpolate
 replace_llama_with_interpolate()
 
-from longchat.train.monkey_patch.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
-replace_llama_attn_with_flash_attn()
+# from longchat.train.monkey_patch.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+# replace_llama_attn_with_flash_attn()
+
+from longchat.train.monkey_patch.llama_xformer_monkey_patch import replace_llama_attn_with_xformer
+replace_llama_attn_with_xformer()
 
 SCRIPT_PATH = Path(__file__).resolve()
-WORKING_DIR = SCRIPT_PATH.parent
-REPO_DIR = WORKING_DIR / Path("../")
+
+REPO_DIR = SCRIPT_PATH.parent
+WORKING_DIR = REPO_DIR / Path("out_eval")
 
 
 def run_lrt_exp(cfgs):
@@ -22,21 +26,29 @@ def run_lrt_exp(cfgs):
 
 
 def run_conv_eval_exp(cfgs, tokenizer):
-    TEST_DIR = WORKING_DIR / Path(f"{cfgs['model_name']}_testcases")
+    TEST_DIR = WORKING_DIR / Path(f"{cfgs['model_name']}_testcases") \
+        if not cfgs["use_fixed_testcases"] else REPO_DIR / Path(cfgs["testcases_dir"])
+
     output_dir = WORKING_DIR / Path(f"{cfgs['model_name']}_predictions")
     test_files = list(TEST_DIR.iterdir())
 
     if not output_dir.exists():
         output_dir.mkdir()
 
+    # load model
+    model = load_model(cfgs["model_name"], cfgs["model_path"], cfgs["local_model"], cfgs["gpu_id"])
+
     for num_topics in cfgs["num_topics"]:
         print(f"**********Start testing {num_topics} topics per prompt**********")
 
-        for file in test_files:
-            if re.search(f"^{num_topics}_topics_{cfgs['question_dist']}", \
-                         str(file.name)) is not None:
-                test_file = file
-                break
+        if not cfgs["use_fixed_testcases"]:
+            for file in test_files:
+                if re.search(f"^{num_topics}_topics_{cfgs['question_dist']}", \
+                            str(file.name)) is not None:
+                    test_file = file
+                    break
+        else:
+            test_file = TEST_DIR / Path(f"{num_topics}_topics.jsonl")
 
         with open(test_file, 'r') as json_file:
             conversation_list = list(json_file)
@@ -47,15 +59,23 @@ def run_conv_eval_exp(cfgs, tokenizer):
         for test_case in conversation_list:
             test_case = json.loads(test_case)
             prompt = test_case["prompt"]
-            prompt_length = test_case["total_length"]
-            topics = test_case["topics"]
-            picked_topics = test_case["picked_topics"]
-            lenth_dist = [float(i)/sum(test_case["length"]) for i in test_case["length"]]
+            
+            if not cfgs["use_fixed_testcases"]:
+                prompt_length = test_case["total_length"]
+                topics = test_case["topics"]
+                picked_topics = test_case["picked_topics"]
+                lenth_dist = [float(i)/sum(test_case["length"]) for i in test_case["length"]]
+            else:
+                topics = test_case["topics"]
+                prompt_length = test_case["prompt_length"]
 
-            response = query_model(cfgs["model_name"], cfgs["model_path"], 
-                                    prompt, tokenizer, cfgs["local_model"],
-                                    cfgs["gpu_id"])
-            summary = f"Label:      {picked_topics}, \nPrediction: {response}, \ntopics:     {topics}, \nprompt_length: {prompt_length}, \nlength_dist: {lenth_dist}\n"
+            response = query_model(cfgs["model_name"], model, 
+                                    prompt, prompt_length, tokenizer, cfgs["gpu_id"])
+
+            if not cfgs["use_fixed_testcases"]:
+                summary = f"Label:      {picked_topics}, \nPrediction: {response}, \ntopics:     {topics}, \nprompt_length: {prompt_length}, \nlength_dist: {lenth_dist}\n"
+            else:
+                summary = f"Label: {topics[0]}, Predict: {response}, --- INFO --- Topics: {topics}, Length: {prompt_length}"
 
             print(summary)
             with open(output_file, "a+") as f:
@@ -68,7 +88,6 @@ def run_conv_eval_exp(cfgs, tokenizer):
             f.close()
         output_file.rename(output_dir / Path(f"{test_file.stem}_{acc}.prediction"))
         # print(f"accuracy: {acc}")
-
 
 def generate_conversations(cfgs, tokenizer):
     conv_list = []
@@ -125,7 +144,8 @@ def main():
     tokenizer = load_tokenizer(cfgs["model_name"], cfgs["model_path"], cfgs["local_model"])
     
     if cfgs["level"] == "easy":
-        generate_conversations(cfgs, tokenizer)
+        if cfgs["generate_conversations"]:
+            generate_conversations(cfgs, tokenizer)
         run_conv_eval_exp(cfgs, tokenizer)
     else:
         run_lrt_exp(cfgs, tokenizer)
---
+++ out_eval/util/lrt_eval.py
@@ -0,0 +1,283 @@
+# part of the code retrieved from https://github.com/anadim/the-little-retrieval-test/blob/main/little_retrieval_test.py
+import yaml
+from yaml import load
+
+import argparse
+import logging
+import random
+import re
+
+import transformers
+import torch
+
+from pathlib import Path
+
+from llms.llm_models import *
+
+def get_tokenizer_model_from_ckpt(cfg):
+    ckpt_path = cfg["ckpt_path"]
+
+    config = transformers.MPTConfig.from_pretrained(ckpt_path)
+    config.attn_config['attn_impl'] = 'torch'
+
+    model = transformers.MPTForCausalLm.from_pretrained(ckpt_path, 
+                                                        config=config, 
+                                                        torch_dtype=torch.bfloat16)
+    # model.to(device='cuda:1')
+    model = model.to(torch.bfloat16)
+
+    tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b",
+                                                            model_max_length=4096,
+                                                            padding_side="right")
+
+    return tokenizer, model
+
+def retrieve_expected(lines, random_line_pos):
+    correct_line = lines[random_line_pos]
+    expected_number = re.search("<\d+>", correct_line)
+    if expected_number is not None:
+        expected_number = int(expected_number.group()[1:-1])
+    else:
+        print(f"Got unparsable line: {correct_line}")
+
+    return expected_number, correct_line
+
+def parse_response(response):
+    raise NotImplementedError
+
+def retrieve_prompt_from_lines(lines):
+    prompt = ""
+    for l in lines:
+        prompt += l
+    
+    return prompt
+
+def block_shuffle(n, B):
+    # Create a list of indices divided into blocks of size B
+    blocks = [list(range(i, min(i + B, n + 1))) for i in range(1, n + 1, B)]
+    # Shuffle the blocks
+    random.shuffle(blocks)
+    # Flatten the list of blocks into a single list of indices
+    shuffled_indices = [i for block in blocks for i in block]
+
+def generate_and_modify_text_file(n, shuffle_flag, B, save_file):
+    """Generates a text file and inserts an execute line at a random position."""
+    lines = [f"Testing Long Context\n\n"]
+    line_numbers = list(range(1, n + 1))
+    if shuffle_flag:
+        #if we want random shuffling, B here allows to shuffle every B lines, and within a block there's no shuffle
+        line_numbers = block_shuffle(n, B)
+
+    lines.extend([f"line {i}: REGISTER_CONTENT is <{random.randint(1, 50000)}>\n" for i in line_numbers])
+    random_line = random.randint(1, n)
+    # add the EXECUTE instruction in random line of the text
+    # lines.insert(len(lines), f"[EXECUTE THIS]: Go to line {random_line} and report only REGISTER_CONTENT, without any context or additional text, just the number, then EXIT\n")
+    lines.insert(len(lines), f"Tell me what is the REGISTER_CONTENT in line {random_line}? I need the number.\n")
+    expected_number, correct_line = retrieve_expected(lines, random_line)
+
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained("mosaicml/mpt-7b")
+    input = tokenizer(retrieve_prompt_from_lines(lines), return_tensors="pt")
+    token_size = input.input_ids.shape[-1]
+
+    if save_file:
+        dir = Path(__file__).parents[0] / Path("lrt_prompts")
+        if not dir.exists():
+            dir.mkdir()
+
+        path =  dir / Path(f"{n}_{token_size}_{expected_number}.lrtpt")
+
+        with open(path, "w") as f:
+            f.writelines(lines)
+    
+    return lines, random_line, expected_number, correct_line, token_size
+
+def generate_original_lrt_lines(n, shuffle_flag, B, save_file):
+    """Generates a text file and inserts an execute line at a random position."""
+    lines = [f"Testing Long Context\n\n"]
+    line_numbers = list(range(1, n + 1))
+    if shuffle_flag:
+        #if we want random shuffling, B here allows to shuffle every B lines, and within a block there's no shuffle
+        line_numbers = block_shuffle(n, B)
+
+    lines.extend([f"line {i}: REGISTER_CONTENT is <{random.randint(1, 50000)}>\n" for i in line_numbers])
+    random_line = random.randint(1, n)
+    # add the EXECUTE instruction in random line of the text
+    lines.insert(random_line - 1, f"[EXECUTE THIS]: Go to line {random_line} and report only REGISTER_CONTENT, without any context or additional text, just the number, then EXIT\n")
+    
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained("mosaicml/mpt-7b")
+    input = tokenizer(retrieve_prompt_from_lines(lines), return_tensors="pt")
+    token_size = input.input_ids.shape[-1]
+
+    for l in lines:
+        if f"line {random_line}: REGISTER_CONTENT is" in l:
+            expected_number = re.search("<\d+>" ,l).group()[1:-1]
+            correct_line = l
+
+    if save_file:
+        dir = Path(__file__).parents[0] / Path("lrt_prompts")
+        if not dir.exists():
+            dir.mkdir()
+
+        path =  dir / Path(f"{n}_{token_size}_{expected_number}.olrtpt")
+    
+        with open(path, "w") as f:
+            f.writelines(lines)
+
+    return lines, random_line, expected_number, correct_line, token_size
+
+def retrieve_cmd_args():
+    parser = argparse.ArgumentParser(
+        prog='lrt_eval',
+        description='lrt_eval'
+    )
+    parser.add_argument('yaml_path')
+    args = parser.parse_args()
+    return args
+
+
+def retrieve_model_response(model, tokenizer, input, lines, random_line_pos, use_gpu):
+    if use_gpu:
+        response = model.generate(input.input_ids.cuda(), max_new_tokens=2048, use_cache=True)
+    else:
+        response = model.generate(input.input_ids, max_new_tokens=2048, use_cache=True)
+    response = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(response[0]))
+    #response = tokenizer.batch_decode([response], skip_special_tokens=True)
+
+    # response_number = re.search("<\d+>.</s>$", response)
+    response_number = re.findall("\d+", response)
+    if response_number is not None:
+        response_number = int(response_number[-1])
+    else:
+        print(f"Got unparsable result: {response}")
+
+    response_line = response
+
+    return response_number, response_line
+
+
+def run_experiment(cfg):
+    model_name = cfg["model_name"]
+    model_path = cfg["model_path"]
+    shuffle_flag = cfg["do_shuffle"]
+    block_size = cfg["block_size"]
+
+    n_values = cfg["n_values"]
+    num_eval_per_len = cfg["num_eval_per_len"]
+
+    use_gpu = cfg["use_gpu"]
+    
+    if model_name == "gpt-3.5-turbo" or "gpt-4":
+        pass
+    elif model_name != "None":
+        tokenizer = transformers.AutoTokenizer.from_pretrained(model_name, use_fast=False)
+        tokenizer.pad_token = tokenizer.unk_token
+        
+        if use_gpu:
+            model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).cuda()
+        else:
+            model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
+    elif model_path != "None":
+        tokenizer = transformers.AutoTokenizer.from_pretrained(model_path, use_fast=False)
+        tokenizer.pad_token = tokenizer.unk_token
+
+        if use_gpu:
+            model = transformers.AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16).cuda()
+        else:
+            model = transformers.AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16)
+    else:
+        tokenizer, model = get_tokenizer_model_from_ckpt(cfg)
+        tokenizer.pad_token = tokenizer.unk_token
+    
+
+    for n in n_values:
+        correct_count = 0
+        n_results = []
+        curr_results = []
+        token_size = -1
+        for i in range(num_eval_per_len):
+            print(f"Running eval {i+1}/{num_eval_per_len} for n = {n}...")
+            
+            # prepare prompt
+            lines, \
+            random_line, \
+            expected_number, \
+            correct_line, _ = generate_and_modify_text_file(n, shuffle_flag, block_size, save_file = cfg["save_prompt"])
+            prompt = retrieve_prompt_from_lines(lines)
+            
+            if model_name == "gpt-3.5-turbo":
+                token_size, \
+                model_output, \
+                incorrect_line = retrieve_from_openai(prompt, model_name)
+            elif model_name == "gpt-4":
+                token_size, \
+                model_output, \
+                incorrect_line = retrieve_from_openai(prompt, model_name)
+            else:
+                input = tokenizer(prompt, return_tensors="pt")
+                token_size = input.input_ids.shape[-1]
+                
+                print(f"Number of tokens: {token_size}")
+
+                # retrieve model output
+                model_output, \
+                incorrect_line = retrieve_model_response(model, tokenizer, input, prompt, random_line, use_gpu)
+            print(f"Expected number in the prompt: {expected_number}, Model output: {model_output}")
+
+            # process results
+            if expected_number == model_output:
+                correct_count += 1
+                n_results.append(1)
+                curr_results.append([1, expected_number, model_output, correct_line, incorrect_line])
+                print("Sweet Success!")
+            else:
+                n_results.append(0)
+                curr_results.append([0, expected_number, model_output, correct_line, incorrect_line])
+                print("Oopsies! Didn't get that one right.")
+                if correct_line:
+                    print(f"Correct result was in this line: {correct_line}")
+                if incorrect_line:
+                    print(f"Model output was found in this line: {incorrect_line}")
+            
+        accuracy = (correct_count / num_eval_per_len) * 100
+        print(f"Accuracy for n = {n}: {accuracy}%")
+        save_results(cfg, curr_results, accuracy, n, token_size)
+
+
+def save_results(cfg, results, accuracy, n, token_size):
+    name = cfg["model_name"]
+    if name == "None":
+        name = cfg["model_path"].split("/")[-1]
+
+    filename = f"{name}_{n}_{token_size}_non_shuffle_{accuracy}.lrt"
+
+    with open(filename, "w") as f:
+        yaml.dump(cfg, f)
+        f.write(f"\naccuracy: {accuracy}\n")
+        f.write(f"token_size: {token_size}\n")
+
+        for r in results:
+            f.write(f"{r}\n")
+
+def main():
+    args = retrieve_cmd_args()
+    f = open(args.yaml_path, "r")
+    eval_cfg = yaml.load(f, Loader=yaml.CLoader)
+    print(yaml.dump(eval_cfg))
+    
+    if eval_cfg["eval_model"]:
+        run_experiment(eval_cfg)
+    else:
+        for n in eval_cfg["n_values"]:
+            generate_original_lrt_lines(n, False, eval_cfg["block_size"], 
+            save_file = eval_cfg["save_prompt"])
+
+
+            # generate_and_modify_text_file(
+            # n, False, eval_cfg["block_size"], 
+            # save_file = True)
+        
+
+if __name__ == "__main__":
+    main()
