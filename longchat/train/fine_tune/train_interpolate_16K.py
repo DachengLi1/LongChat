045526b4890480ec64308cdc7c85d5baf0813ce0
Dacheng Li <dal177@ucsd.edu>
2023-06-25 23:23:59 +0400

    refactor

--- longchat/train/fine_tune/train_interpolate_16K.py
+++ longchat/train/fine_tune/train_interpolate_16K.py
@@ -3,7 +3,7 @@
 # Need to call this before importing transformers.
 from longchat.train.monkey_patch.llama_interpolate_monkey_patch import replace_llama_with_interpolate
 
-replace_llama_with_interpolate()
+replace_llama_with_interpolate(ratio=8)
 
 from longchat.train.monkey_patch.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
