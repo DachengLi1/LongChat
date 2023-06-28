8a78fbeb18771e4e30b0ee2674667ccf4d7dd49d
Ying Sheng <sqy1415@gmail.com>
2023-06-28 19:00:06 +0000

    update results

--- evaluation/zeroscrolls/run.sh
+++ evaluation/zeroscrolls/run.sh
@@ -1,11 +1,11 @@
 #scrolls (200 validation samples)
-python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_32K_interpolate" --flash --dataset qasper --ratio 16
-python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_7b_16K" --flash --dataset qasper
-python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_13b_16K" --flash --dataset qasper
- 
-python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_32K_interpolate" --flash --dataset narrative_qa --ratio 16
-python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_7b_16K" --flash --dataset narrative_qa
-python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_13b_16K" --flash --dataset narrative_qa
+# python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_32K_interpolate" --flash --dataset qasper --ratio 16
+# python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_7b_16K" --flash --dataset qasper
+# python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_13b_16K" --flash --dataset qasper
+#  
+# python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_32K_interpolate" --flash --dataset narrative_qa --ratio 16
+# python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_7b_16K" --flash --dataset narrative_qa
+# python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/dacheng-data/longchat_13b_16K" --flash --dataset narrative_qa
 
 python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/vicuna-7b-v1.3" --flash --dataset qasper
 python eval.py --dataset-version "tau/scrolls" --model-name-or-path "/home/haozhang/LongChat/data/vicuna-7b-v1.3" --flash --dataset narrative_qa
